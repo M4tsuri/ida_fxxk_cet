13db11ecf390279ffd697c141f04bdd0f538ff9f
Asada Shino <ctsinon@live.com>
2020-10-01 22:17:38 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # ida_fxxk_cet
 解决IDA Pro 7.0版本在遇到使用CET技术的ELF文件时符号解析错误的问题
+
+症状：打开ELF文件时IDA报错：`Unexpected plt stub`，且符号解析不正确，如下图所示（2020 ciscn final Day1 server）：
+
+![](https://tva1.sinaimg.cn/large/007S8ZIlgy1gja6y49g95j31c00u0npd.jpg)
+
+使用方法：在file->Script File...中运行该脚本。
+
+效果如下：
+
+![](https://tva1.sinaimg.cn/large/007S8ZIlgy1gja70djvtmj31c00u0b29.jpg)
---
+++ fxxk_cet.py
@@ -0,0 +1,73 @@
+import idc
+import idautils
+from elftools.elf.elffile import ELFFile, RelocationSection
+import ida_nalt
+from elftools.elf.sections import Symbol
+
+def log(x): print("[+] " + x)
+
+def get_plt_sec():
+    log("Checking cet...")
+    for seg in idautils.Segments():
+        if idc.SegName(seg) == '.plt.sec':
+            log("CET found.")
+            return seg
+
+    return None
+
+def resolve_func_name(funcea, rela_plt, dynsym):
+    dism_addr = list(idautils.FuncItems(funcea))
+    endbr = idc.GetMnem(dism_addr[0])
+    assert (endbr == 'endbr64' or endbr == 'endbr32')
+    jmp = dism_addr[1]
+    assert(idc.GetMnem(jmp) == 'jmp')
+    assert(idc.GetOpType(jmp, 0) == idc.o_mem)
+
+    func_plt = next(idautils.DataRefsFrom(idc.GetOperandValue(jmp, 0)))
+    dism_addr = list(idautils.FuncItems(func_plt))
+    endbr = idc.GetMnem(dism_addr[0])
+    assert (endbr == 'endbr64' or endbr == 'endbr32')
+    push = dism_addr[1]
+    assert(idc.GetMnem(push) == 'push')
+    assert(idc.GetOpType(push, 0) == idc.o_imm)
+    rela_idx = idc.GetOperandValue(push, 0)
+    
+    sym_idx = list(rela_plt.iter_relocations())[rela_idx].entry['r_info_sym']
+    return '_' + str(dynsym.get_symbol(sym_idx).name)
+    
+
+def load_section(elf, name):
+    tmp = elf.get_section_by_name(name)
+    if not tmp:
+        log("%s not found, this file may be damaged." % name)
+        return
+    log(".rela.plt found.")
+    return tmp
+
+
+def start():
+    file_path = ida_nalt.get_input_file_path()
+    with open(file_path, 'rb') as f:
+        elf = ELFFile(f)
+        log("ELF file %s loaded." % file_path)
+    
+        plt_sec = get_plt_sec()
+        if not plt_sec:
+            log("No CET found, this elf does not need to recover.")
+            return
+    
+        rela_plt = load_section(elf, ".rela.plt")
+        dynsym = load_section(elf, '.dynsym')
+        
+        for funcea in idautils.Functions(plt_sec, idc.SegEnd(plt_sec)):
+            real_name = resolve_func_name(funcea, rela_plt, dynsym)
+            origin_name = idc.GetFunctionName(funcea)
+            idc.MakeName(funcea, real_name)
+            log("Function %s renamed to %s." % (origin_name, real_name))
+
+
+if __name__ == '__main__':
+    start()
+    
+    
+    
